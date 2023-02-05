ef039f2c0a6082c98ff223431bda70e527cc814d
Roman Sonin <truant@duck.com>
2023-02-05 14:31:44 +0000

    parser refactoring

--- bot_in_progress.py
+++ bot_in_progress.py
@@ -2,55 +2,90 @@ import telegram
 import telegram.ext
 import re
 from random import randint
-from parser_latest import show_latest
+from parser_latest import showNextMovie
+'''from parser_latest import present_movie'''
 
-# The API Key we received for our bot
-API_KEY = "5622509365:AAEyXvYJMad28r0So26WQrFhbXvDQTnfy60"
-# Create an updater object with our API Key
+# Ключ API от BotFather:
+API_KEY = "<PUT_YOUR_KEY_HERE>"
+# Создаем апдейтер с ключем API:
 updater = telegram.ext.Updater(API_KEY)
-# Retrieve the dispatcher, which will be used to add handlers
+# Создаем диспетчер, с помощью которого добаляются обработчики:
 dispatcher = updater.dispatcher
-# Our states, as integers
-ABOUT = 0
-RELEASES = 1
-CANCEL = 2
+# Состояния в виде чисел:
+WELCOME = 0
+MOVIE = 1
+SETTINGS = 2
+CANCEL = 3
 
-n = 2
-url = "https://whereyouwatch.com/latest-reports/?pg=" + str(n)
-
-# The entry function
-def about(update_obj, context):
-    # send the welcoming message, and show the keyboard markup (suggested answers)
-    first_name = update_obj.message.from_user['first_name']
-    update_obj.message.reply_text(show_latest(url, 1),
-        reply_markup=telegram.ReplyKeyboardMarkup([['latest', 'previous']], one_time_keyboard=True)
+# Начальная функция:
+def start(update_obj, context):
+    # Отправляем сообщение и выводим кнопки для дальнейшего взаимодействия:
+    update_obj.message.reply_text("Перейти в настройки или посмотреть последний репорт?",
+        reply_markup=telegram.ReplyKeyboardMarkup([['Настройки', 'Репорт']], one_time_keyboard=True)
     )
-    # go to the RELEASES state
-    return RELEASES
+    # Переходим в состояние WELCOME:
+    return WELCOME
+
+# Вспомогательная функция, отправляющая последний репорт:
+def send_report(update_obj, context):
+    # store the numbers in the context
+    context.user_data['last_movie'] = showNextMovie
+    # send the question
+    update_obj.message.reply_text("Вот последний репорт:")
+
+# В состоянии WELCOME проверить, выбрал ли пользователь посмотреть последний репорт:
+def welcome(update_obj, context):
+    if update_obj.message.text.lower() in ['Репорт']:
+        # Отправляет репорт и переходит в состояние MOVIE:
+        send_report(update_obj, context)
+        return MOVIE
+    else:
+        # Уходит в состояние CANCEL:
+        return CANCEL
+
+# В состоянии MOVIE:
+def next_movie(update_obj, context):
+    # Команда для следующего репорта:
+    more_movies = 'Еще'
+    # Проверяет, была ли дана команда для следующего репорта:
+    if more_movies == str(update_obj.message.text):
+        # Зацыкливает в состояние MOVIE:
+        send_report(update_obj, context)
+        return MOVIE
+    else:
+        # При любом другом ответе:
+        update_obj.message.reply_text("Возвращаю назад")
+        # Возвращает в состояние WELCOME:
+        return WELCOME
 
 def cancel(update_obj, context):
-    # get the user's first name
-    first_name = update_obj.message.from_user['first_name']
-    update_obj.message.reply_text(
-        f"Okay, no question for you then, take care, {first_name}!", reply_markup=telegram.ReplyKeyboardRemove()
+    update_obj.message.reply_text('Выход', reply_markup=telegram.ReplyKeyboardRemove()
     )
     return telegram.ext.ConversationHandler.END
 
-# a regular expression that matches latest or previous
-latest_previous_regex = re.compile(r'^(latest|previous|l|p)$', re.IGNORECASE)
-# Create our ConversationHandler, with only one state
+# Пустое состояние SETTINGS,(TODO):
+def settings(update_obj, context):
+    x = 'state'
+
+# Создаем ConversationHandler с единственным состоянием:
+yes_no_regex = re.compile(r'^(yes|no|y|n)$', re.IGNORECASE)
+report = 'Репорт'
+more = 'Еще'
+bye = 'Выход'
 handler = telegram.ext.ConversationHandler(
-      entry_points=[telegram.ext.CommandHandler('start', about)],
+      entry_points=[telegram.ext.CommandHandler('start', start)],
       states={
-            ABOUT: [telegram.ext.MessageHandler(telegram.ext.Filters.regex(latest_previous_regex), about)],
-            # RELEASES: [telegram.ext.MessageHandler(telegram.ext.Filters.regex(r'^\d+$'), releases)],
-            # CANCEL: [telegram.ext.MessageHandler(telegram.ext.Filters.regex(latest_previous_regex), cancel)],
+            WELCOME: [telegram.ext.MessageHandler(telegram.ext.Filters.regex(report), welcome)],
+            MOVIE: [telegram.ext.MessageHandler(telegram.ext.Filters.regex(more), next_movie)],
+            CANCEL: [telegram.ext.MessageHandler(telegram.ext.Filters.regex(bye), cancel)],
+            SETTINGS: [telegram.ext.MessageHandler(telegram.ext.Filters.regex(yes_no_regex), settings)],
       },
       fallbacks=[telegram.ext.CommandHandler('cancel', cancel)],
       )
-# add the handler to the dispatcher
+# Обработчик для диспетчера:
 dispatcher.add_handler(handler)
-# start polling for updates from Telegram
+
+# Метод, ожидающий апдейтов от Телеграма:
 updater.start_polling()
-# block until a signal (like one sent by CTRL+C) is sent
+# Работать до прерывания:
 updater.idle()
--- parser_latest.py
+++ parser_latest.py
@@ -2,57 +2,73 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-source = "https://whereyouwatch.com/latest-reports/?pg="
-page = 2
-url = source + str(page)
+# Counter of pages on site and movies in list:
+def pages_and_movies_counter():
+    cntPage = 1 # page counter (1 to inf)
+    cntMovie = 0 # movie counter (0 to 4, only 5 movies on a page)
+    while True:
+        yield cntPage, cntMovie # generator object (page, movie)
+        cntMovie += 1
+        if cntMovie > 4: # then movie counter exceeds 5:..
+            cntPage += 1 # ..page counter turns page..
+            cntMovie = 0 # ..and resets movie counter
 
-response = requests.get(url)
-html = response.content
+counter = pages_and_movies_counter() # class generator
+counterTuple = next(counter) # class tuple
+pageNumber = counterTuple[0] # class int
+movieNumber = counterTuple[1] # class int
 
-soup = BeautifulSoup(html, "html.parser")
-latest_reports = soup.find_all(class_="jrResourceContent")
+# Constructing source url for soup and scraping:
+def source_constructor(page_number):
+    base = "https://whereyouwatch.com/latest-reports/?pg="
+    source = base + str(page_number)
+    return source
 
-def page_counter():
-    pageNum = 1
-    return str(pageNum)
+url = source_constructor(pageNumber) # class str
 
-def return_multiple():
-    return [1,2,3,4,5,6,7]
+# Source scraping by attribute <jrResourceContent> on a page:
+def page_scraper(source_url):
+    response = requests.get(source_url)
+    html = response.content
+    soup = BeautifulSoup(html, "html.parser")
+    scraped_reports = soup.find_all(class_="jrResourceContent") # class set
+    return scraped_reports
 
+scrapedPage = page_scraper(url) # class bs4.element.ResultSet
 
-# придется чото думать с кортежами:
-'''def show_latest(source, page):
-    latest_movies = []
-
-    # page_counter = 1
-    # movie_counter = 0
-
-    for report in latest_reports:
-        movie = tuple()
-
-        report_name = report.find(class_="jrResourceTitle")
-        report_movie = report.find(class_="jrResourceListingTitle")
-        report_link = report_movie.find('a', attrs={'href': re.compile("^https://")})
-        submition_username = report.find(class_="jrResourceUserLabel")
-        submition_userlink = submition_username.find('a', attrs={'href': re.compile("^https://")})
-        submition_time = report.find(class_="jrResourceCreatedDate")
-        report_description = report.find(class_="jrResourceDescription")
-        
-        movie.append(report_name.get_text().strip('\n'))
-        movie.append(report_movie.get_text().strip('\n'))
-        movie.append(report_link.get('href'))
-        movie.append(submition_username.get_text().strip('\n'))
-        movie.append(submition_userlink.get('href'))
-        movie.append(submition_time.get_text().strip('\n'))
-        if str(type(report_description)) == "<class 'NoneType'>":
-            movie.append('No Description')
+# Movie parsing to a 5-item list from scraped page:
+def page_parser(scraped_page):
+    raw_movies_list = [] # list of movies, 5 items
+    for report in scraped_page:
+        raw_movie_data = [] # single movie as a list of variables, 7 items
+        # Variables parsing from scraped page:
+        report_name = report.find(class_="jrResourceTitle") # report name
+        report_movie = report.find(class_="jrResourceListingTitle") # movie title
+        report_link = report_movie.find('a', attrs={'href': re.compile("^https://")}) # link to report page
+        submition_username = report.find(class_="jrResourceUserLabel") # reporter name
+        submition_userlink = submition_username.find('a', attrs={'href': re.compile("^https://")}) # ink to reporter page
+        submition_time = report.find(class_="jrResourceCreatedDate") # report date
+        report_description = report.find(class_="jrResourceDescription") # report description
+        # Variables formatting:
+        raw_movie_data.append(report_name.get_text().strip('\n'))
+        raw_movie_data.append(report_movie.get_text().strip('\n'))
+        raw_movie_data.append(report_link.get('href'))
+        raw_movie_data.append(submition_username.get_text().strip('\n'))
+        raw_movie_data.append(submition_userlink.get('href'))
+        raw_movie_data.append(submition_time.get_text().strip('\n'))
+        if str(type(report_description)) == "<class 'NoneType'>": # if report description is absent:..
+            raw_movie_data.append('No Description') # ..put empty value to keep constant length of list
         else:
-            movie.append(report_description.get_text().strip('\n'))
-        
-        latest_movies.append(movie)
-    
-    for movie in latest_movies:
-        return movie
+            raw_movie_data.append(report_description.get_text().strip('\n'))
+        raw_movies_list.append(raw_movie_data)
+    return raw_movies_list
 
-print(show_latest(url, page))'''
+parsedPage = page_parser(scrapedPage) # class list
 
+# Yields single formatted movie:
+def present_movie(list):
+    for movie in parsedPage:
+        yield movie
+
+showMovie = present_movie(parsedPage) # class generator
+showNextMovie = next(showMovie) # class list
